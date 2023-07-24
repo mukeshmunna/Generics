c7d28cb31c0651d6bcd2c00a2f8cf7a02212f533
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-24 16:23:53 +0530

    [Mukesh] add: created to find the maximum

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace FindMaximum
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Operation maximum = new Operation();
+
+            maximum.FindMaxInteger(1, 2, 4);
+
+
+        }
+
+    }
+
+
+
+}
