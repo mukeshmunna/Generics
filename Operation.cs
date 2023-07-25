6686157c1f6c7a44478b007eeded7e73a9999dbc
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-25 22:32:26 +0530

    [Mukesh] add: created for generic multiple

--- Operation.cs
+++ Operation.cs
@@ -4,61 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FindMaximum
+namespace TestMaximum
 {
     public class Operation
     {
-        public void FindMaximumInteger(int a, int b, int c)
-
+        public void FindMaxInteger(int a, int b, int c)
         {
-
-            if (a >= b && a <= c)
-
-                Console.WriteLine("a in max");
-
-            if (b >= a && b <= c)
-
-                Console.WriteLine("b in max");
-
-            if (c >= b && c <= a)
-
-                Console.WriteLine("c in max");
-
+            if (a >= b && a >= c)
+                Console.WriteLine("a in maximum");
+            if (b >= a && b >= c)
+                Console.WriteLine("b in maximum");
+            if (c >= b && c >= a)
+                Console.WriteLine("c in maximum");
         }
         public void FindMaxFloat(float a, float b, float c)
-
         {
-
-            if (a >= b && a <= c)
-
-                Console.WriteLine("a in max");
-
-            if (b >= a && b <= c)
-
-                Console.WriteLine("b in max");
-
-            if (c >= b && c <= a)
-
-                Console.WriteLine("c in max");
-
+            if (a >= b && a >= c)
+                Console.WriteLine("a in maximum");
+            if (b >= a && b >= c)
+                Console.WriteLine("b in maximum");
+            if (c >= b && c >= a)
+                Console.WriteLine("c in maximum");
         }
         public void FindMaxString(string a, string b, string c)
-
         {
-
             if (a.CompareTo(b) >= 0 && a.CompareTo(c) >= 0)
-
-                Console.WriteLine("a in max");
-
+                Console.WriteLine("a in maximum");
             if (b.CompareTo(a) >= 0 && b.CompareTo(c) >= 0)
-
-                Console.WriteLine("b in max");
-
+                Console.WriteLine("b in maximum");
             if (c.CompareTo(b) >= 0 && c.CompareTo(a) >= 0)
-
-                Console.WriteLine("c in max");
-
+                Console.WriteLine("c in maximum");
         }
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ internal class Program
         Console.WriteLine("Finding the maximum of three");
         GenericClass<int> operation = new GenericClass<int>();
         operation.FindMaximum(1, 2, 3);
+        GenericClass<float> operation1 = new GenericClass<float>();
+        operation1.FindMaximum(1.1f, 2.1f, 3.1f);
+        GenericClass<string> operation2 = new GenericClass<string>();
+        operation2.FindMaximum("a", "b", "c");
 
     }
 }
